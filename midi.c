ff80e72c82999beb59019a7f7388d530dd9198bd
Nicholas Parkanyi <n.parkanyi@gmail.com>
2015-08-29 00:49:44 -0400

    remove printf from midi.c

--- midi.c
+++ midi.c
@@ -168,7 +168,6 @@ int MIDITrack_load(MIDITrack * track, FILE * file){
   //if id is not "MTrk", not a track
   for (i = 0; i < 4; i++){
     if (track->header.id[i] != name[i]){
-      printf("MTrk not found\n");
       return FILE_INVALID;
     }
   }
